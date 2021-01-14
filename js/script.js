0aa666a33bf56169bdc2029fd100f1e6671841f5
Zainul Abideen <zainul.a@zen3tech.com>
2021-01-14 18:26:38 +0530

    Form Textual Changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function ($) {
-  
   $("#your-profile").attr("enctype", "multipart/form-data");
   $("#your-profile").attr("encoding", "multipart/form-data");
   renderMarkup();
@@ -14,17 +13,16 @@ jQuery(document).ready(function ($) {
     console.log("birthday", birthday);
     console.log("underAgeValidate(birthday)", underAgeValidate(birthday));
     const id = $(this).data("id");
-    if(underAgeValidate(birthday)){
-      $(`#dob_err_${id}`).html("Age cannot be less than 18")
-    } else{
-      $(`#dob_err_${id}`).html("")
+    if (underAgeValidate(birthday)) {
+      $(`#dob_err_${id}`).html("Age cannot be less than 18");
+    } else {
+      $(`#dob_err_${id}`).html("");
     }
   });
 
   $("#team_size").change((e) => {
     const numberOfParticipants = parseInt(e.target.value) || 1;
     renderMarkup(numberOfParticipants);
-    disableFutureDates()
   });
 
   $("#password, #confirm_password").keyup(() => {
@@ -70,7 +68,7 @@ function underAgeValidate(birthday) {
   } else {
     return false;
   }
-} 
+}
 
 const renderParticipatForm = (id) => `
     <div class="cm__participant__title">
@@ -80,12 +78,12 @@ const renderParticipatForm = (id) => `
     <div class="participant-form">
         <div class="field__group_3">
         <div class="cm__input__feild">
-            <label class="cm__feild__label">Participant Name</label>
+            <label class="cm__feild__label">Name</label>
             <input class="cm__input" type="text" name="participant_name_${id}" id="participant_name_${id}" required/>
         </div>
         
         <div class="cm__input__feild">
-            <label class="cm__feild__label">Participant profession</label>
+            <label class="cm__feild__label">Education Status</label>
             <select class="cm__input" name="participant_profession_${id}" id="participant_profession_${id}" required>
                 <option value="">Please select</option>
                 <option value="studying">Currently studying</option>
@@ -95,7 +93,7 @@ const renderParticipatForm = (id) => `
         </div>
 
         <div class="cm__input__feild">
-			<label class="cm__feild__label">Participant DOB</label>
+			<label class="cm__feild__label">DOB</label>
             <input class="cm__input dob" type="date" name="participant_dob_${id}" id="participant_dob_${id}" data-id="${id}" required/>
             <div class="error" id="dob_err_${id}"></div>
         </div>
@@ -132,7 +130,7 @@ const renderParticipatForm = (id) => `
         <input class="cm__input" type="date" name="graduation_date_${id}" id="graduation_date_${id}" required/></div>
         
         <div class="cm__input__feild">
-        <label class="cm__feild__label">Participant's Email</label>
+        <label class="cm__feild__label">Email</label>
         <input class="cm__input" type="text" name="participant_email_${id}" id="participant_email_${id}" required/></div>
         
         <div class="cm__input__feild">
@@ -142,23 +140,23 @@ const renderParticipatForm = (id) => `
 
         <div class="field__group_3">
         <div class="cm__input__feild">
-        <label class="cm__feild__label">Gov issued Photo ID card</label>
+        <label class="cm__feild__label">Gov Issued Photo ID Card</label>
         <input class="cm__input" type="file" name="photo_id_card_${id}" id="photo_id_card_${id}" required/></div>
 
         <div class="cm__input__feild">
-        <label class="cm__feild__label">Participant's contact number</label>
+        <label class="cm__feild__label">Contact Number</label>
 <div class="cm__input__feild__number__wrap">
 		<span class="cm__input__contact">+91</span>
         <input class="cm__input cm__input__contact__number" pattern="[789][0-9]{9}" title="Please enter a valid 10 digit mobile number" type="text" name="participant_contact_number_${id}" id="participant_contact_number_${id}" required/></div></div>
 
         <div class="cm__input__feild">
-        <label class="cm__feild__label">Participants City</label>
+        <label class="cm__feild__label">City</label>
         <input class="cm__input" type="text" name="participants_city_${id}" id="participants_city_${id}" required/></div>
         </div>
         
         <div class="field__group_3">
         <div class="cm__input__feild">
-        <label class="cm__feild__label">Participants Nationality</label>
+        <label class="cm__feild__label">Nationality</label>
         <input class="cm__input" type="text" name="participants_nationality_${id}" id="participants_nationality_${id}" required/></div>
 
         <div class="cm__input__feild">
